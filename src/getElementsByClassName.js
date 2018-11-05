d772b98f20564530babd9ad972eaf0f4e3b315bf
cherrih <cherri.hartigan@gmail.com>
2018-11-05 14:54:00 -0800

    working implementation

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var nodes = [];
+  
+  var element = document.body;
+  // console.log(`this is document body`, document.body)
+  
+  var getClasses = function(element){
+    
+    if (element.classList && element.classList.contains(className)){
+      nodes.push(element);
+    }
+    
+    if (element.childNodes){
+      element.childNodes.forEach(function(item){
+        getClasses(item);
+      });
+    }
+  };
+  
+  getClasses(element);
+  return nodes;
 };
+
+
+//You should use document.body, element.childNodes, and element.classList
